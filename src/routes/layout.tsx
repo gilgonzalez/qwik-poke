e55c0db82d777ddf398c184bbdd86a18785a626b
gil gonzalez <fragilgon@gmail.com>
2023-05-18 21:18:14 +0200

    boton guapo

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,8 +1,6 @@
 import { component$, Slot, useStyles$ } from '@builder.io/qwik';
-import Navbar from '~/components/shared/navbar/navbar';
-
 import styles from './styles.css?inline';
-import { PokemonProvider } from '~/context';
+
 
 
 export default component$(() => {
@@ -10,11 +8,6 @@ export default component$(() => {
 
 
   return (
-    <PokemonProvider>
-      <Navbar />
-      <main class="flex flex-col items-center justify-center">
-        <Slot />
-      </main>
-    </PokemonProvider>
+    <Slot/>
   );
 });
