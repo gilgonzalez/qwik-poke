103a7d6c398ba5d428ba77389e4fe47d1eb5681a
gil gonzalez <fragilgon@gmail.com>
2023-05-19 23:13:56 +0200

    descargar en windows

---
+++ src/routes/counter/layout.tsx
@@ -0,0 +1,17 @@
+import { Slot, component$ } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <>
+    <div class="flex flex-col justify-center items-center mt-10">
+        <Slot />
+
+        <Link class="mt-10 hover:text-gray-100 btn hover:shadow-amber-50 shadow transition-all duration-300 hover:bg-pink-500 bg-pink-700" href="/">
+            Go back
+        </Link>
+    </div>
+    </>
+    
+  )
+});
